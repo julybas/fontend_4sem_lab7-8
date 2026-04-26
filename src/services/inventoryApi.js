42f00cbbbeb460a6becfcd3a3bee05a0be9087c1
unknown <yaroslav@gmail.com>
2026-04-26 19:06:36 +0300

    setup axios API service for inventory crud

---
+++ src/services/inventoryApi.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:5000"; // Заміни на реальний URL
+
+export const inventoryApi = {
+  // Отримати весь список
+  getAll: () => axios.get(`${API_URL}/inventory`),
+
+  // Отримати одну позицію
+  getById: (id) => axios.get(`${API_URL}/inventory/${id}`),
+
+  // Створити нову позицію (multipart/form-data)
+  create: (formData) => axios.post(`${API_URL}/register`, formData),
+
+  // Оновити текст (JSON)
+  updateText: (id, data) => axios.put(`${API_URL}/inventory/${id}`, data),
+
+  // Оновити фото (multipart/form-data)
+  updatePhoto: (id, formData) =>
+    axios.put(`${API_URL}/inventory/${id}/photo`, formData),
+
+  // Видалити позицію
+  delete: (id) => axios.delete(`${API_URL}/inventory/${id}`),
+};
